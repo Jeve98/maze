b2649d8c3f97e4c377fc36ca031bad897ab46871
Jeve98 <102717437+Jeve98@users.noreply.github.com>
2022-05-04 14:41:17 +0900

    Define 방향기

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 // 숫자 배열 선언 후 길 뚫기 0==길 1==블럭, if(arr[i]==1) draw ▒▒;
 
 #include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+#define direction 224
+#define space 32
 
 using namespace std;
 
