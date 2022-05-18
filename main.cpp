11a514d0732735abf534148beb8dbb418eaf1ca8
Jeve98 <102717437+Jeve98@users.noreply.github.com>
2022-05-18 18:35:58 +0900

    22.05.18
    maze 생성을 위한 난수 및 stack 헤더파일
    
    maze size defined
    
    세부 설정 변경 및 이후 아이템 획득을 위한 설정값 추가
    
    maze의 출입구 생성을 위한 origin x,y 생성
    
    maze 생성 및 이동 함수 생성
    
    main 함수 동작 모습 변경
    
    maze 생성, 작성
    user 위치 최신화
    user와 wall 충돌 시 이동 불가 및 user 이동

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
+ㅁ#include <random>
+#include <stack>
 
 #define direction 224
 #define space 32
+#define mazeSize 41
 
 using namespace std;
 
@@ -14,26 +17,67 @@ enum gameState{
     giveUp=2
 };
 enum menu{
-    start=1,
-    quit=-1
+    start=-1,
+    quit=1
 };
 enum keyboard{
-    up=72,
-    left=75,
-    right=77,
-    down=80
+    Up=72,
+    Left=75,
+    Right=77,
+    Down=80
+};
+enum mazeState{
+    wall=0,
+    street=1,
+    origin=2,
+    destination=3,
+};
+enum crush{
+    //crushItem=1, item 추가시 사용
+    crushDestination=2,
+    notcrush=3
 };
 
-void previewGame(); // 대기화면에서 user의 선택 확인
+int maze[mazeSize][mazeSize]={0, };
+int origin_x=0;
+int origin_y=0;
+
+menu previewGame(); // 대기화면에서 user의 선택 확인
 void drawPreview(); // 대기화면 로드
 void drawUserCusor(int y); // 대기화면에 유저 커서 로드
 void gotoxy(int x, int y);
 gameState startGame(); // game 결과 반환
 void makeMaze(); // 미로 생성
-void drawMap(int *maze); // game 화면 로드
+void drawMap(); // game 화면 로드
+int makeRandom(int a, int b); // random값 생성
+crush userMove(); // user 움직임
+bool testInside(int type); // 움직임이 maze 내부에 있는지 검사
 
 int main() {
-    previewGame();
+    menu state;
+
+    while (1){
+        state=previewGame();
+        if(state==start){
+            for(int i=0; i<mazeSize; i++){
+                for(int j=0; j<mazeSize; j++){
+                    maze[i][j]=wall;
+                }
+            }
+
+            if(startGame()==giveUp){
+                cout<<"End Game";
+                return 0;
+            }
+            else{
+                cout<<"Clear";
+                return 0;
+            } // Clear시 시간 출력
+        }
+        else if(state==quit){
+            return 0;
+        }
+    }
 
     return 0;
 }
@@ -83,4 +127,242 @@ void drawPreview(){
 void drawUserCusor(int y){
     gotoxy(9,9+y);
     cout<<">";
+}
+
+int makeRandom(int a, int b){
+    int random=0;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis(a, b);
+    random=dis(gen);
+
+    return random;
+}
+
+void makeMaze(){
+    stack<int> x;
+    stack<int> y;
+    int startPoint=makeRandom(1, mazeSize-2);
+    int mazeDirection[4]={0,};
+    int Direction=makeRandom(1,4);
+    int i=2, j=startPoint;
+    int mid_i=0, mid_j=0;
+    int testing_number=1;
+    int tempX=0, tempY=0;
+    int desti= makeRandom(1, mazeSize);
+
+    mazeDirection[Direction-1]=1;
+    maze[0][startPoint]=origin;
+    maze[2][startPoint]=street;
+    maze[1][startPoint]=street;
+    origin_x=startPoint;
+    origin_y=0;
+    x.push(j);
+    y.push(i);
+
+    do{
+        tempX=j;
+        tempY=i;
+        switch(Direction){
+            case 1: i+=2; mid_i=-1; mid_j=0; break;    // 아래
+            case 2: j+=2; mid_i=0; mid_j=-1; break;    // 오른쪽
+            case 3: i-=2; mid_i=1; mid_j=0; break;   // 위
+            case 4: j-=2; mid_i=0; mid_j=1; break;   // 왼쪽
+        }
+        if((i>0)&&(i<mazeSize-1)&&(j>0)&&(j<mazeSize-1)){ //안에서만 놀아
+            if(maze[i][j]==wall){
+                for(int ii=0; ii<4; ii++){
+                    mazeDirection[ii]=0;
+                }
+
+                x.push(j);
+                y.push(i);
+                maze[i][j]=street;
+                maze[i+mid_i][j+mid_j]=street;
+
+                Direction= makeRandom(1,4);
+            }
+            else{
+                mazeDirection[Direction-1]=1;
+
+                testing_number=1;
+                for(int ii=0; ii<4; ii++){
+                    if(mazeDirection[ii]==0){
+                        i=tempY;
+                        j=tempX;
+                        Direction=ii+1;
+                        mazeDirection[ii]=1;
+                        testing_number=0;
+                        break;
+                    }
+                }
+
+                if(testing_number==1){
+                    i=y.top();
+                    y.pop();
+                    j=x.top();
+                    x.pop();
+                    Direction= makeRandom(1,4);
+                }
+            }
+        }
+        else{
+            mazeDirection[Direction-1]=1;
+
+            testing_number=1;
+            for(int ii=0; ii<4; ii++){
+                if(mazeDirection[ii]==0){
+                    i=tempY;
+                    j=tempX;
+                    Direction=ii+1;
+                    mazeDirection[ii]=1;
+                    testing_number=0;
+                    break;
+                }
+            }
+
+            if(testing_number==1){
+                i=y.top();
+                y.pop();
+                j=x.top();
+                x.pop();
+                Direction= makeRandom(1,4);
+            }
+        }
+    }while(!(x.empty()));
+
+    for(int ii=0; ; ii++){
+        if(maze[mazeSize-3][ii]==street){
+            desti--;
+            if(desti==0){
+                maze[mazeSize-1][ii]=destination;
+                maze[mazeSize-2][ii]=street;
+                break;
+            }
+            else if(ii>=mazeSize-2){
+                ii=0;
+            }
+        }
+    }
+}
+
+void drawMap(){
+    int x=0, y=0;
+    for(int i=0; i<mazeSize; i++){
+        gotoxy(x,y++);
+        for(int j=0; j<mazeSize; j++){
+            if(maze[i][j]==wall){
+                cout<<"▒▒";
+            }
+            else if(maze[i][j]==street||maze[i][j]==destination){
+                cout<<"  ";
+            }
+            else if(maze[i][j]==origin){
+                cout<<"ㅇ";
+            }
+        }
+        cout<<"\n";
+    }
+}
+
+gameState startGame(){
+    makeMaze();
+    while(1){
+        system("cls");
+        drawMap();
+        if(userMove()==crushDestination){
+            return clear;
+        }
+    }
+}
+
+crush userMove(){
+    int input=0;
+    input=_getch();
+
+    if(input==direction){
+        input=_getch();
+        if(testInside(input)){
+
+            switch (input) {
+                case Up: if(maze[origin_y-1][origin_x]==street){
+                        origin_y--;
+                        maze[origin_y][origin_x]=origin;
+                        maze[origin_y+1][origin_x]=street;
+                        return notcrush;
+                    }
+                    else if(maze[origin_y-1][origin_x]==destination){
+                        return crushDestination;
+                    }
+                    else{
+                        return notcrush;
+                    }
+                case Down: if(maze[origin_y+1][origin_x]==street){
+                        origin_y++;
+                        maze[origin_y][origin_x]=origin;
+                        maze[origin_y-1][origin_x]=street;
+                        return notcrush;
+                    }
+                    else if(maze[origin_y+1][origin_x]==destination){
+                        return crushDestination;
+                    }
+                    else{
+                        return notcrush;
+                    }
+                case Right: if(maze[origin_y][origin_x+1]==street){
+                        origin_x++;
+                        maze[origin_y][origin_x]=origin;
+                        maze[origin_y][origin_x-1]=street;
+                        return notcrush;
+                    }
+                    else if(maze[origin_y][origin_x+1]==destination){
+                        return crushDestination;
+                    }
+                    else{
+                        return notcrush;
+                    }
+                case Left: if(maze[origin_y][origin_x-1]==street){
+                        origin_x--;
+                        maze[origin_y][origin_x]=origin;
+                        maze[origin_y][origin_x+1]=street;
+                        return notcrush;
+                    }
+                    else if(maze[origin_y][origin_x-1]==destination){
+                        return crushDestination;
+                    }
+                    else{
+                        return notcrush;
+                    }
+            }
+        }
+    }
+}
+
+bool testInside(int type){
+    switch (type) {
+        case Up: if(origin_y-1<0){
+            return false;
+            }
+            else{
+                return true;
+            }
+        case Down: if(origin_y+1>=mazeSize){
+            return false;
+            }
+            else{
+                return true;
+            }
+        case Right: if(origin_x+1>=mazeSize){
+            return false;
+            }
+            else{
+                return true;
+            }
+        case Left: if(origin_x-1<0){
+            return false;
+            }
+            else{
+                return true;
+            }
+    }
 }
